fb37da435f412f1e79a0759f0ce2fe2e11bec95f
Sohui-Park <73065191+Sohui-Park@users.noreply.github.com>
2022-02-17 20:07:52 +0900

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -41,8 +41,8 @@ const GenerateMessage = name => {
   ];
   const d = new Date();
   const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
-  // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
+  return `안녕하세요 ${name}님🐾 \n 하단 메뉴 중 원하시는 메뉴를 클릭해주세요. 첫 메뉴로 돌아가시려면 '시작'을 입력하시면 됩니다😊 `; // your message
+  // My message   return `안녕하세요 ${name}님🐾 \n 하단 메뉴 중 원하시는 메뉴를 클릭해주세요. \n 첫 메뉴로 돌아가시려면 '시작'을 입력하시면 됩니다�😊 `;
 };
 
 module.exports = AutoDM;
